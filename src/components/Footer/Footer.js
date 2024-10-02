4578409395cb3a271d31e2c0bc7d7e053b2c2411
Andres <aaplv2@gmail.com>
2024-10-02 14:11:11 -0300

    Maquetacion footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,9 +1,42 @@
 import React from "react";
+import { SocialIcon } from "react-social-icons";
+import "react-social-icons/instagram";
 
 function Footer() {
   return (
-    <div>
-      <p>Footer</p>
+    <div className="footer">
+      <ul className="footer__media">
+        <li className="footer__media-item">
+          <SocialIcon
+            className="footer__media-instagram"
+            url="https://instagram.com/swingtiago"
+            fgColor="#fff"
+            bgColor="none"
+          ></SocialIcon>
+        </li>
+        <li className="footer__media-item">
+          <SocialIcon
+            className="footer__media-facebook"
+            url="https://www.facebook.com/swingtiago"
+            fgColor="#fff"
+            bgColor="none"
+          ></SocialIcon>
+        </li>
+        <li className="footer__media-item">
+          <SocialIcon
+            className="footer__media-youtube"
+            url="https://www.youtube.com/channel/UCghhZJLAiRz6dyNo8vvmYMw"
+            fgColor="#fff"
+            bgColor="none"
+          ></SocialIcon>
+        </li>
+      </ul>
+      <div className="footer__about">
+        <p className="footer__about-title">
+          Swing y Lindy Hop en Santiago de Chile
+        </p>
+        <a className="footer__about-mail">info@swingtiago.com</a>
+      </div>
     </div>
   );
 }

8afcb5350947e26a823e23ed370fcf756ed19352
Andres <aaplv2@gmail.com>
2024-10-02 17:22:16 -0300

    Maqutacion main

--- src/components/EventCard/EventCard.js
+++ src/components/EventCard/EventCard.js
@@ -1,9 +1,32 @@
 import React from "react";
 
+import imageMainEventPath from "../../images/PRUEBA_proximo_evento.jpg";
+
 function EventCard() {
   return (
-    <div>
-      <p>EventCard</p>
+    <div className="eventCard">
+      <div className="eventCard__titles">
+        <p className="eventCard__titles-subtitle">Próximo Evento</p>
+        <p className="eventCard__titles-title">Título del evento</p>
+      </div>
+      <div className="eventCard__event">
+        <img
+          className="eventCard__event-image"
+          src={imageMainEventPath}
+          alt="Próximo evento"
+        />
+        <p className="eventCard__event-description">
+          Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry. Lorem Ipsum has been the industry's standard dummy text ever
+          since the 1500s, when an unknown printer took a galley of type and
+          scrambled it to make a type specimen book. It has survived not only
+          five centuries, but also the leap into electronic typesetting,
+          remaining essentially unchanged. It was popularised in the 1960s with
+          the release of Letraset sheets containing Lorem Ipsum passages, and
+          more recently with desktop publishing software like Aldus PageMaker
+          including versions of Lorem Ipsum.
+        </p>
+      </div>
     </div>
   );
 }
--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,9 +1,19 @@
 import React from "react";
+import EventCard from "../EventCard/EventCard";
 
 function Main() {
   return (
-    <div>
-      <p>Main</p>
+    <div className="main">
+      <EventCard></EventCard>
+      <div className="main__scrollbar">
+        <p>Barra horizontal con eventos</p>
+      </div>
+      <div className="main__calendar">
+        <p>Calendario</p>
+      </div>
+      <div className="main__calendarScroll">
+        <p>Calendarios de mes pasado o próximo</p>
+      </div>
     </div>
   );
 }

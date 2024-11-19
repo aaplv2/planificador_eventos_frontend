f4684d81639d7b888b314ad135e64daacdd6f91c
Andres <aaplv2@gmail.com>
2024-11-18 23:48:52 -0300

    cambiar ruta events

--- src/actions/getAllEvents.js
+++ src/actions/getAllEvents.js
@@ -1,7 +1,7 @@
 import { actionFecth } from "./actionFetch";
 
 export async function getAllEvents() {
-  return actionFecth("/events", "GET", {
+  return actionFecth("/api/events", "GET", {
     "Content-Type": "application/json",
     Authorization: "Bearer " + localStorage.getItem("jwt") || "",
   });

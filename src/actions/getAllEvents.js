cd18cd3a28732e8180216791eafa55e6850f89b4
Andres <aaplv2@gmail.com>
2024-11-18 23:53:39 -0300

    correccion ruta

--- src/actions/getAllEvents.js
+++ src/actions/getAllEvents.js
@@ -1,7 +1,7 @@
 import { actionFecth } from "./actionFetch";
 
 export async function getAllEvents() {
-  return actionFecth("/api/events", "GET", {
+  return actionFecth("/events", "GET", {
     "Content-Type": "application/json",
     Authorization: "Bearer " + localStorage.getItem("jwt") || "",
   });

5fd80552b3c24cac364eb98683a0f525b3b9480b
Andres <aaplv2@gmail.com>
2024-11-20 18:13:13 -0300

    hardcode getAllEvents

--- src/actions/getAllEvents.js
+++ src/actions/getAllEvents.js
@@ -1,8 +1,17 @@
 import { actionFecth } from "./actionFetch";
 
 export async function getAllEvents() {
-  return actionFecth("/events", "GET", {
-    "Content-Type": "application/json",
-    Authorization: "Bearer " + localStorage.getItem("jwt") || "",
+  // return actionFecth("/events", "GET", {
+  //   "Content-Type": "application/json",
+  //   Authorization: "Bearer " + localStorage.getItem("jwt") || "",
+  // });
+  return fetch("https://planificador-eventos-backend.vercel.app/events", {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: "Bearer " + localStorage.getItem("jwt") || "",
+    },
+  }).then((res) => {
+    return res.json();
   });
 }

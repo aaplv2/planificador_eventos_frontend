6d17c7dd623a4a482caa5d1931f344fdaaca4bc4
Andres <aaplv2@gmail.com>
2024-11-20 17:43:48 -0300

    hardcode register

--- src/actions/register.js
+++ src/actions/register.js
@@ -1,15 +1,26 @@
 import { actionFecth } from "./actionFetch";
 
 export async function register(name, email, password) {
-  return actionFecth(
-    "/signup",
-    "POST",
-    {
+  // return actionFecth(
+  //   "/signup",
+  //   "POST",
+  //   {
+  //     Accept: "application/json",
+  //     "Content-Type": "application/json",
+  //   },
+  //   JSON.stringify({ name, email, password })
+  // )
+  return fetch("https://planificador-eventos-backend.vercel.app/signup", {
+    method: "POST",
+    headers: {
       Accept: "application/json",
       "Content-Type": "application/json",
     },
-    JSON.stringify({ name, email, password })
-  )
+    body: JSON.stringify({ name, email, password }),
+  })
+    .then((res) => {
+      return res.json();
+    })
     .then((res) => {
       return res;
     })

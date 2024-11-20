a6d0c3c0daa7d27ab2a9768d229173b5b251a2f0
Andres <aaplv2@gmail.com>
2024-11-20 18:22:15 -0300

    hardcode updateUserInfo

--- src/actions/updateUserInfo.js
+++ src/actions/updateUserInfo.js
@@ -1,16 +1,29 @@
 import { actionFecth } from "./actionFetch";
 
 export async function updateUserInfo(name, email) {
-  return actionFecth(
-    "/users/me",
-    "PATCH",
-    {
+  // return actionFecth(
+  //   "/users/me",
+  //   "PATCH",
+  //   {
+  //     "Content-Type": "application/json",
+  //     Authorization: "Bearer " + localStorage.getItem("jwt") || "",
+  //   },
+  //   JSON.stringify({
+  //     name,
+  //     email,
+  //   })
+  // );
+  return fetch("https://planificador-eventos-backend.vercel.app/users/me", {
+    method: "PATCH",
+    headers: {
       "Content-Type": "application/json",
       Authorization: "Bearer " + localStorage.getItem("jwt") || "",
     },
-    JSON.stringify({
+    body: JSON.stringify({
       name,
       email,
-    })
-  );
+    }),
+  }).then((res) => {
+    return res.json();
+  });
 }

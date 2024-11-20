dd4b2bd453fa3d4654f18f3a97744800b3dd51d1
Andres <aaplv2@gmail.com>
2024-11-20 18:10:12 -0300

    hardcode getUser

--- src/actions/getUser.js
+++ src/actions/getUser.js
@@ -1,9 +1,21 @@
 import { actionFecth } from "./actionFetch";
 
 export async function getUser(token) {
-  return actionFecth("/users/me", "GET", {
-    Accept: "application/json",
-    "Content-Type": "application/json",
-    Authorization: `Bearer ${token}`,
-  }).then((data) => data);
+  // return actionFecth("/users/me", "GET", {
+  //   Accept: "application/json",
+  //   "Content-Type": "application/json",
+  //   Authorization: `Bearer ${token}`,
+  // })
+  return fetch("https://planificador-eventos-backend.vercel.app//users/me", {
+    method: "GET",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+  })
+    .then((res) => {
+      return res.json();
+    })
+    .then((data) => data);
 }

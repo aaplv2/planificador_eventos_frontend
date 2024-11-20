dfc18a97d3b5d4d128e3c3aad186960a7d568fda
Andres <aaplv2@gmail.com>
2024-11-20 18:17:44 -0300

    hardcode getEventsByDate

--- src/actions/getEventsByDate.js
+++ src/actions/getEventsByDate.js
@@ -5,8 +5,22 @@ var customParseFormat = require("dayjs/plugin/customParseFormat");
 dayjs.extend(customParseFormat);
 
 export async function getEventsByDate(date) {
-  return actionFecth(`/events/${encodeURIComponent(date)}`, "GET", {
-    "Content-Type": "application/json",
-    Authorization: "Bearer " + localStorage.getItem("jwt") || "",
+  // return actionFecth(`/events/${encodeURIComponent(date)}`, "GET", {
+  //   "Content-Type": "application/json",
+  //   Authorization: "Bearer " + localStorage.getItem("jwt") || "",
+  // });
+  return fetch(
+    `https://planificador-eventos-backend.vercel.app/events/${encodeURIComponent(
+      date
+    )}`,
+    {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: "Bearer " + localStorage.getItem("jwt") || "",
+      },
+    }
+  ).then((res) => {
+    return res.json();
   });
 }

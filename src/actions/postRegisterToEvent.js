54fff71adb8c327fdbad0bc23f8c95ae12eb4fb9
Andres <aaplv2@gmail.com>
2024-11-20 18:19:38 -0300

    hardcode postRegisterToEvent

--- src/actions/postRegisterToEvent.js
+++ src/actions/postRegisterToEvent.js
@@ -1,13 +1,26 @@
 import { actionFecth } from "./actionFetch";
 
 export async function postRegisterToEvent(body, id) {
-  return actionFecth(
-    `/events/${id}/attendees`,
-    "PATCH",
+  // return actionFecth(
+  //   `/events/${id}/attendees`,
+  //   "PATCH",
+  //   {
+  //     "Content-Type": "application/json",
+  //     Authorization: "Bearer " + localStorage.getItem("jwt") || "",
+  //   },
+  //   JSON.stringify(body)
+  // );
+  return fetch(
+    `https://planificador-eventos-backend.vercel.app/events/${id}/attendees`,
     {
-      "Content-Type": "application/json",
-      Authorization: "Bearer " + localStorage.getItem("jwt") || "",
-    },
-    JSON.stringify(body)
-  );
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: "Bearer " + localStorage.getItem("jwt") || "",
+      },
+      body: JSON.stringify(body),
+    }
+  ).then((res) => {
+    return res.json();
+  });
 }

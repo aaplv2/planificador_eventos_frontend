fbb561d44bcd0d0e792c8cd2bcf9f9971c2d76e5
Andres <aaplv2@gmail.com>
2024-11-20 18:20:36 -0300

    hardcode postTaskToEvent

--- src/actions/postTaskToEvent.js
+++ src/actions/postTaskToEvent.js
@@ -1,14 +1,26 @@
 import { actionFecth } from "./actionFetch";
 
 export async function postTaskToEvent(body, id) {
-  return actionFecth(
-    `/events/${id}/tasks`,
-    "PATCH",
+  // return actionFecth(
+  //   `/events/${id}/tasks`,
+  //   "PATCH",
+  //   {
+  //     "Content-Type": "application/json",
+  //     Authorization: "Bearer " + localStorage.getItem("jwt") || "",
+  //   },
+  //   JSON.stringify(body)
+  // );
+  return fetch(
+    `https://planificador-eventos-backend.vercel.app/events/${id}/tasks`,
     {
-      "Content-Type": "application/json",
-      Authorization: "Bearer " + localStorage.getItem("jwt") || "",
-    },
-    JSON.stringify(body)
-  );
-
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: "Bearer " + localStorage.getItem("jwt") || "",
+      },
+      body: JSON.stringify(body),
+    }
+  ).then((res) => {
+    return res.json();
+  });
 }

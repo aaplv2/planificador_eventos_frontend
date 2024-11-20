9500f6887c54f77a35787c21879a9a4db0d316bf
Andres <aaplv2@gmail.com>
2024-11-20 18:16:13 -0300

    hardcode deleteEventById

--- src/actions/deleteEventById.js
+++ src/actions/deleteEventById.js
@@ -1,8 +1,20 @@
 import { actionFecth } from "./actionFetch";
 
 export async function deleteEventById(id) {
-  actionFecth(`/events/date/${id}`, "DELETE", {
-    "Content-Type": "application/json",
-    Authorization: "Bearer " + localStorage.getItem("jwt") || "",
+  // actionFecth(`/events/date/${id}`, "DELETE", {
+  //   "Content-Type": "application/json",
+  //   Authorization: "Bearer " + localStorage.getItem("jwt") || "",
+  // });
+  return fetch(
+    `https://planificador-eventos-backend.vercel.app/events/date/${id}`,
+    {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: "Bearer " + localStorage.getItem("jwt") || "",
+      },
+    }
+  ).then((res) => {
+    return res.json();
   });
 }

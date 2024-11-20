a33068a052c904e19989ec1e9ffce15a49e4199b
Andres <aaplv2@gmail.com>
2024-11-20 18:15:25 -0300

    hardcode getEventById

--- src/actions/getEventById.js
+++ src/actions/getEventById.js
@@ -1,8 +1,20 @@
 import { actionFecth } from "./actionFetch";
 
 export async function getEventById(id) {
-  return actionFecth(`/events/date/${id}`, "GET", {
-    "Content-Type": "application/json",
-    Authorization: "Bearer " + localStorage.getItem("jwt") || "",
+  // return actionFecth(`/events/date/${id}`, "GET", {
+  //   "Content-Type": "application/json",
+  //   Authorization: "Bearer " + localStorage.getItem("jwt") || "",
+  // });
+  return fetch(
+    `https://planificador-eventos-backend.vercel.app/events/date/${id}`,
+    {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: "Bearer " + localStorage.getItem("jwt") || "",
+      },
+    }
+  ).then((res) => {
+    return res.json();
   });
 }
